2d73aed6bd5e248896916995b06c21c713021bcd
David_Qian_0504 <94476615+RSQ0504@users.noreply.github.com>
2024-03-19 10:39:32 -0700

    backip

--- estimate_color_model.m
+++ estimate_color_model.m
@@ -83,6 +83,8 @@ function representation_score_map = weight_pixel(image,color_model)
 end
 
 function [seed_pixel_x,seed_pixel_y] = select_seed_pixel(image, color_bin_mask)
+    % color_bin_mask 是一个和image 一样大小的2维mask,
+    % 用来标出处于color_bin中的pixel在image里的位置
     seed_pixel = 0;
     % TODO: 返回 pixel的 xy坐标
 end
