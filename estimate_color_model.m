58aa6b6f7fb4b7b59b0b1693f49991f53df7281b
David_Qian_0504 <94476615+RSQ0504@users.noreply.github.com>
2024-03-19 09:11:16 -0700

    2 todo

--- estimate_color_model.m
+++ estimate_color_model.m
@@ -16,16 +16,29 @@ function color_model = estimate_color_model(image, tau)
 
         mask = bins_mask(max_bin,:,:);
         
-        seed_pixel = select_seed_pixel(image, mask);
+        [seed_pixel_x,seed_pixel_y] = select_seed_pixel(image, mask);
+        seed_pixel = image(seed_pixel_x,seed_pixel_y);
         seed_pixels = [seed_pixels seed_pixel];
         
-        % TODO: The paper is saying that after they select the color bin they want to add to the color model, they select the seed pixel using Eq. 11. After they have the seed pixel, they compute the weights of a guided filter centered at that pixel and use that to fit the normal distribution. The guided filter will have higher weights for pixels that are close in color and proximity to the center pixel. So basically they are determining the parameters of the normal distribution based on a local neighborhood of similar pixels around the seed pixel if that makes sense. (edited) 
-        new = ?
+        % The paper is saying that after they select the color bin they want to add to the color model, they select the seed pixel using Eq. 11. After they have the seed pixel, they compute the weights of a guided filter centered at that pixel and use that to fit the normal distribution. The guided filter will have higher weights for pixels that are close in color and proximity to the center pixel. So basically they are determining the parameters of the normal distribution based on a local neighborhood of similar pixels around the seed pixel if that makes sense. (edited) 
+        
+        new = get_new_layer(image,seed_pixel_x,seed_pixel_y);
         color_model = [color_model; new];
         
         representation_score = weight_pixel(image,color_model);
 
     end
+end
+
+
+function [weighted_mean,weighted_std] = get_new_layer(image,seed_pixel_x,seed_pixel_y)
+    neighborhood = image(max(1, seed_pixel_x - 10) : min(size(image, 1), seed_pixel_x + 10), ...
+                    max(1, seed_pixel_y - 10) : min(size(image, 2), seed_pixel_y + 10));
+    guided_weights = guidedfilter(rgb2gray(image), neighborhood);
+    weighted_mean = sum(sum(neighborhood .* guided_weights)) / sum(sum(guided_weights));
+    weighted_variance = sum(sum((neighborhood - weighted_mean).^2 .* guided_weights)) / sum(sum(guided_weights));
+    weighted_std = sqrt(weighted_variance);
+
 end
 
 function [votes,bins_mask] = calculate_votes(image, bins_mask,representation_score,tau)
@@ -62,14 +75,14 @@ function representation_score_map = weight_pixel(image,color_model)
             for i = 1: len(color_model)
                 for j = i+1 : len(color_mode)
                     representation_score_map(r,c) = min(representation_score_map(r,c),min)
-                    % TODO:
+                    % TODO: 完成式子
                 end
             end
         end
     end
 end
 
-function seed_pixel = select_seed_pixel(image, mask)
+function [seed_pixel_x,seed_pixel_y] = select_seed_pixel(image, mask)
     seed_pixel = 0;
-    % TODO:
+    % TODO: 返回 pixel的 xy坐标
 end
