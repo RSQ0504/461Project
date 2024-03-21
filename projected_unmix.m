e7ff1dfd97fd73eec5a2415ea3288986e2e8a201
David_Qian_0504 <94476615+RSQ0504@users.noreply.github.com>
2024-03-21 14:04:32 -0700

    fix

--- projected_unmix.m
+++ projected_unmix.m
@@ -4,7 +4,7 @@ function F_hat = projected_unmix(mu1, std1, mu2, std2, c)
     u_hat1 = c - ((c - mu1) * n') * n / norm(n)^2; % Projection for layer 1
     u_hat2 = c - ((c - mu2) * n') * n / norm(n)^2; % Projection for layer 2
 
-    alpha_hat1 = norm(c - u_hat2) / norm(u_hat1 - u_hat2);
+    alpha_hat1 = norm(c - mu2) / norm(mu1 - mu2);
     alpha_hat2 = 1 - alpha_hat1;
 
     cost1 = layer_color_cost(u_hat1, mu1, std1);
