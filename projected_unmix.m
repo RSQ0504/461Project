c74b42a5b680dad7684b05560593c4c1a038c145
rqa4 <rqa4@sfu.ca>
2024-03-22 01:18:45 -0700

    backup

--- projected_unmix.m
+++ projected_unmix.m
@@ -1,4 +1,4 @@
-function F_hat = projected_unmix(mu1, cov1, mu2, cov, c)
+function F_hat = projected_unmix(mu1, cov1, mu2, cov2, c)
     n = mu1 - mu2;
 
     u_hat1 = c - ((c - mu1) * n') * n / norm(n)^2; % Projection for layer 1
@@ -8,7 +8,7 @@ function F_hat = projected_unmix(mu1, cov1, mu2, cov, c)
     alpha_hat2 = 1 - alpha_hat1;
 
     cost1 = layer_color_cost(u_hat1, mu1, cov1);
-    cost2 = layer_color_cost(u_hat2, mu2, cov);
+    cost2 = layer_color_cost(u_hat2, mu2, cov2);
 
     F_hat = alpha_hat1 * cost1 + alpha_hat2 * cost2;
 end
