33f97d199326f31d9602d9133008e675a5ce4c04
David_Qian_0504 <94476615+RSQ0504@users.noreply.github.com>
2024-03-27 13:11:26 -0700

    backup

--- projected_unmix.m
+++ projected_unmix.m
@@ -9,7 +9,7 @@ function [F_hat, alpha_hat1, alpha_hat2, u_hat1, u_hat2] = projected_unmix(mu1, 
 
     cost1 = layer_color_cost(u_hat1, mu1, cov1);
     cost2 = layer_color_cost(u_hat2, mu2, cov2);
-    if alpha_hat1 >= 1
+    if alpha_hat1 >= 1 || alpha_hat1 <= 0
         F_hat = Inf;
     else
         F_hat = alpha_hat1 * cost1 + alpha_hat2 * cost2;
