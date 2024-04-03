21b126a68ecbebc56a28f33d36584d5649888111
David.Q <rqa4@sfu.ca>
2024-04-03 05:09:25 -0700

    backup

--- extract_layers.m
+++ extract_layers.m
@@ -4,10 +4,10 @@ function color_layers = extract_layers(image, color_model, min_F_hat_layers, alp
     for r = 1:rows
         for c = 1:cols
             model_info = min_F_hat_layers(r,c,:);
-            model1 = color_model(model_info(1):model_info(1)+2,:);
+            model_info(1) = (model_info(1)-1)/3*4+1;
+            model_info(2) = (model_info(2)-1)/3*4+1;
             % mean1 = model1(:,1);
             if model_info(1) ~= model_info(2)
-                model2 = color_model(model_info(2):model_info(2)+2,:);
                 color_layers(r,c,model_info(2):model_info(2)+2) = u_hat_2(r,c,:);
                 color_layers(r,c,model_info(2)+3) = alphas_2(r,c);
                 % mean2 = model2(:,1);
@@ -17,7 +17,7 @@ function color_layers = extract_layers(image, color_model, min_F_hat_layers, alp
         end
     end
 
-    for i = 1:3:size(color_model,1)
+    for i = 1:4:size(color_model,1)*(4/3)
         temp = color_layers(:,:,i:i+3);
         %imshow(temp);
         %imwrite(temp,sprintf('result_self%02d.png',i),'jpg','Quality',95);
