6c61b5cbf675e38fda9e7446d9f8e03b3493eb83
David.Q <rqa4@sfu.ca>
2024-04-15 11:46:08 -0700

    backup[

--- weight_pixel.m
+++ weight_pixel.m
@@ -2,7 +2,7 @@ function [representation_score, min_F_hat_layers, alphas_1, alphas_2, u_hat_1, u
     [rows, cols, ~] = size(image);
     num_layers_times_3 = size(color_model,1);
     min_F_hat_layers = zeros(rows, cols, 2);
-    representation_score = Inf * ones(rows, cols);
+    rp = Inf * ones(1,rows* cols);
     alphas_1 = zeros(rows, cols);
     alphas_2 = zeros(rows, cols);
     u_hat_1 = zeros(rows, cols, 3);
@@ -14,8 +14,7 @@ function [representation_score, min_F_hat_layers, alphas_1, alphas_2, u_hat_1, u
         covi = model1(:, 2 : end);
         ui_mat = repmat(ui, [1, rows*cols]);
         cost = layer_color_cost(V, ui_mat, covi);
-        cost = reshape(cost,[rows,cols]);
-        representation_score = cost;
+        rp = cost;
         min_F_hat_layers = ones(rows, cols, 2);
         alphas_1 = ones(rows, cols);
         alphas_2 = zeros(rows, cols);
@@ -28,7 +27,6 @@ function [representation_score, min_F_hat_layers, alphas_1, alphas_2, u_hat_1, u
             covi = model1( : , 2 : end);
             ui_mat = repmat(ui, [1, rows*cols]);
             cost = layer_color_cost(V, ui_mat, covi);
-            cost = reshape(cost,[rows,cols]);
             for j = i + 3 : 3 : num_layers_times_3
                 model2 = color_model(j : j + 2, :);
                 uj= model2( : , 1);
@@ -36,28 +34,33 @@ function [representation_score, min_F_hat_layers, alphas_1, alphas_2, u_hat_1, u
                 covj = model2( : , 2 : end);
                 [project_score, alpha_i, alpha_j, u_hat1, u_hat2] = projected_unmix(ui_mat, covi, uj_mat, covj, V);
                 % TODO:
-                if cost <= min(project_score, representation_score(r, c))
-                    % fprintf("layer %d in case 1\n", i)
+                temp = min(project_score, rp);
+                filter = find(cost <= temp);
+                for idx = 1:length(filter)
+                    [r, c] = ind2sub([rows, cols], filter(idx));
                     min_F_hat_layers(r, c, 1) = i;
                     min_F_hat_layers(r, c, 2) = i;
                     alphas_1(r, c) = 1;
                     alphas_2(r, c) = 0;
-                    u_hat_1(r,c,:) = pixel;
-                    representation_score(r, c) = cost;
-                    % u_hat_2(r,c,:) = pixel;
-                elseif representation_score(r, c) >= project_score 
+                    u_hat_1(r,c,:) = image(r,c,:);
+                end
+                rp(filter) = cost(filter);
+                filter = find(rp >= project_score);
+                for idx = 1:length(filter)
+                    [r, c] = ind2sub([rows, cols], filter(idx));
                     % fprintf("layer %d in case 2 with %d\n", i, j)
                     min_F_hat_layers(r, c, 1) = i;
                     min_F_hat_layers(r, c, 2) = j;
-                    alphas_1(r, c) = alpha_i;
-                    alphas_2(r, c) = alpha_j;
-                    u_hat_1(r,c,:) = u_hat1;
-                    u_hat_2(r,c,:) = u_hat2;
-                    representation_score(r, c) = project_score;
+                    alphas_1(r, c) = alpha_i(idx);
+                    alphas_2(r, c) = alpha_j(idx);
+                    u_hat_1(r,c,:) = u_hat1(idx);
+                    u_hat_2(r,c,:) = u_hat2(idx);
+                    rp(idx) = project_score(idx);
                 end
-                representation_score(r, c) = min([representation_score(r, c), cost, project_score]);
+                rp = min(min(rp, cost), project_score);
             end
         end
     end
+    representation_score = reshape(rp,[rows,cols]);
 
 end
