d7306abea45d677043329ac4953663132139c7bc
David_Qian_0504 <94476615+RSQ0504@users.noreply.github.com>
2024-04-16 15:18:42 -0700

    backup

--- smoothing.m
+++ smoothing.m
@@ -11,6 +11,14 @@ function [U_final,Alpha_final] = smoothing(input,U_temp,Alpha_temp,color_model)
     normal = sum(Alpha_final,1);
     normal = repmat(normal, [Num_layer,1, 1]);
     Alpha_final = Alpha_final ./ normal;
+    
+    for r = 1:rows
+        for c= 1:cols
+            if any(Alpha_temp(:,r,c,:)==1)
+                Alpha_final(:,r,c,:) = Alpha_temp(:,r,c,:);
+            end
+        end
+    end
 
     normal = zeros(size(input));
     for i = 1:Num_layer
@@ -25,13 +33,15 @@ function [U_final,Alpha_final] = smoothing(input,U_temp,Alpha_temp,color_model)
         end
     end
     % hi = zeros(size(input));
-    normal = normal ./ input;
+    normal = input ./ normal;
+    normal(isnan(normal)) = 0; % todo this is right
     for k = 1:size(U_final, 1)
-        U_final(k, :, :, :) = squeeze(U_final(k, :, :, :)) ./ normal;
+        U_final(k, :, :, :) = squeeze(U_final(k, :, :, :)) .* normal;
         u = squeeze(U_final(k,:,:,:));
         Alpha = squeeze(Alpha_final(k,:,:,:));
         imwrite(u, sprintf('result_self%02d.png',k), 'png', 'Alpha', Alpha);
         % hi = hi + squeeze(U_final(k, :, :, :)) .* squeeze(repmat(Alpha_final(k,:,:,:),[1,1,1,3]));
+        % imshow(sprintf('result_self%02d.png',k));
     end
     % imshow(uint8(hi.*255));
 end
